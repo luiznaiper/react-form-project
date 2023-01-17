47a703a61cf71e31e8d05acce70087e127d2d694
luiznaiper <luiz_naiper@hotmail.com>
2023-01-17 08:51:08 -0300

    :construction: Almos finished form

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,61 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useState } from 'react';
+import Button from './components/Button';
+import Card from './components/Card';
+import Container from './components/Container';
+import Input from './components/Input';
+import useForm from './hooks/useForm';
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const [users, setUsers] = useState([]);
 
+  const [form, handleChange, reset] = useForm({
+    name: '',
+    lastName: '',
+    email: '',
+  });
+  const submitForm = (e) => {
+    e.preventDefault();
+    setUsers([...users, form]);
+    reset();
+  };
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
-}
+    <Container>
+      <Card>
+        <div style={{ padding: 20 }}>
+          <form onSubmit={submitForm}>
+            <Input
+              label="Name"
+              name="name"
+              value={form.name}
+              onChange={handleChange}
+            />
+            <Input
+              label="Last Name"
+              name="lastName"
+              value={form.lastName}
+              onChange={handleChange}
+            />
+            <Input
+              label="Email"
+              name="email"
+              value={form.email}
+              onChange={handleChange}
+            />
+            <Button>Enviar</Button>
+          </form>
+        </div>
+      </Card>
+      <Card>
+        <ul>
+          {users.map((user) => (
+            <li key={user.email}>
+              {user.name} {user.lastName} : {user.email}
+            </li>
+          ))}
+        </ul>
+      </Card>
+    </Container>
+  );
+};
 
-export default App
+export default App;
